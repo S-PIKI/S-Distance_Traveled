9c788b64acea9c4f25b5552a9599a903fd9abece
S-PIKI <pallansanchez1232@gmail.com>
2025-03-30 20:15:07 -0500

    Create Distance_Traveled.py

---
+++ Distance_Traveled.py
@@ -0,0 +1,18 @@
+# Author: Allan S
+# CIS-190
+# Program: Practical 1 - Distance Traveled
+
+#Formula
+# Distance = speedMilesPerHour X Time
+
+# Speed of the car in miles per hour
+speedMilesPerHour = 70
+
+# Calculate and display distances
+print("=" * 30)
+print(f'The distance the car will travel in 6 hours is: {speedMilesPerHour * 6:,} Miles')
+print("-" * 30)
+print(f'The distance the car will travel in 10 hours is: {speedMilesPerHour * 10:,} Miles')
+print("-" * 30)
+print(f'The distance the car will travel in 15 hours is: {speedMilesPerHour * 15:,} Miles')
+print("=" * 30)
